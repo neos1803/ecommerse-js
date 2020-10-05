5002f104eb9a528f90483d19488a5b9354a6a7fb
subhandp <subhan.dinda.putra>
2020-10-05 08:19:04 +0800

    initial

--- be/src/config/config.js
+++ be/src/config/config.js
@@ -16,11 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "jdmlhtxexohgah",
-    "password": "68a4badfca040ec02f3fbe040703bcffca6ed2207d70ff985eed669482f3f0d6",
-    "database": "dbqa04be2v3n43",
-    "host": "ec2-54-246-85-151.eu-west-1.compute.amazonaws.com",
-    "dialect": "postgres",
-    "port": "5432"
+    "username": "xDnjqTTQ7Q",
+    "password": "LV5vLVxKdF",
+    "database": "xDnjqTTQ7Q",
+    "host": "remotemysql.com",
+    "dialect": "mysql",
   }
 }
